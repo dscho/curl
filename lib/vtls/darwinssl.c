016ca936ca9966a1016a8f4314d27e1e46276f06
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-06-23 12:30:28 +0200

    fixup! vtls: move md5sum into the Curl_ssl struct

--- lib/vtls/darwinssl.c
+++ lib/vtls/darwinssl.c
@@ -2712,9 +2712,9 @@ CURLcode Curl_darwinssl_random(struct Curl_easy *data, uunsigned char *entropy,
 }
 
 static CURlcode Curl_darwinssl_md5sum(unsigned char *tmp, /* input */
-				      size_t tmplen,
-				      unsigned char *md5sum, /* output */
-				      size_t md5len)
+                                      size_t tmplen,
+                                      unsigned char *md5sum, /* output */
+                                      size_t md5len)
 {
   (void)md5len;
   (void)CC_MD5(tmp, (CC_LONG)tmplen, md5sum);
